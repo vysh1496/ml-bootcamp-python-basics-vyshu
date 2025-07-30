b303c8e8bb0787795effff720a03c79d042884b1
vysh1496 <vyshnavi1496@gmail.com>
2025-07-30 13:48:57 +0530

    adding all files

---
+++ app4.py
@@ -0,0 +1,5 @@
+number=10
+scores=[10,20,30]
+names=["Alexa","siri","jarvis"]
+for i in range(number):
+    print(i)
---
+++ app5.py
@@ -0,0 +1,5 @@
+scores = [10, 20, 30]
+
+for score in scores:
+    print(score)
+
