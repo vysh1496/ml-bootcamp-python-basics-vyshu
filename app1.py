ce1fef180218d63822cfb8cb01ad4304011a51fb
vysh1496 <vyshnavi1496@gmail.com>
2025-07-30 13:45:39 +0530

    uploading app1.py

---
+++ app1.py
@@ -0,0 +1,12 @@
+name="MLWEC"
+number=10.0
+isOn=True
+letter="H"
+# Arithemetic- + - * / %
+#logical -and,or,not
+#comparision- <,>,==,!=,<=,>=
+#assignment - =,+=,-=,*=,/=
+if (number>5):
+    print(name)
+else:
+    print(isOn)    
